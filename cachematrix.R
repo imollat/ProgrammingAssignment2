e4b61cc68b783776088fb1b3c14a55a27252a9b4
INGOLF MOLLAT <Ingolf@INGOLFs-iMac.local>
2014-07-21 13:11:16 +0200

    Added some validity checking.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 ## The following functions will allow the creation of a cachable matrix object with the
 ## ability to cache its inverse. It can be used for optimization purposes in which the
-## inverse is calculated only once for a static matrix.
+## inverse is calculated only once for an unchanged matrix.
 
 ## The following function expects a square numerical matrix as input and will create
 ## a new object that not only caches the original matrix, but also its inverse, once
 ## calculated and set.
 makeCacheMatrix <- function(x = matrix()) {
+
+    # check if input x is not valid
+    if(!validEntry(x)) { return(NULL) }
+        
     im <- NULL
     set <- function(y) {
-        x <<- y
-        im <<- NULL
+        if(validEntry(y)) { 
+            x <<- y
+            im <<- NULL
+        }
     }
     get <- function() x
     setinverse <- function(inverse) im <<- inverse
@@ -19,6 +25,25 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
+## The following function does some simple checks on whether x is a square inversable matrix
+validEntry <- function(x) {
+    # check if x not a matrix...
+    if (!is.matrix(x)) {
+        message("not a matrix")
+        return(FALSE)
+    }
+    # ...and whether it is not square...
+    else if (nrow(x) != ncol(x)) {
+        message("matrix not square")
+        return(FALSE)
+    } # ...and determinant equal to zero (not inversable)
+    else if (det(x) == 0) {
+        message("matrix is singular")
+        return(FALSE)
+    }    
+    
+    return(TRUE)    
+}
 
 ## The following function will take a cacheable matrix object (as created via the
 ## makeCacheMatrix function) and return its inverse. Depending on whether it has been done
